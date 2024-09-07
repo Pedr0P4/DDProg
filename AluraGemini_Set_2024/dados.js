508bc9671832bbab91d7a3a478d098e23573f82e
Pedro Paulo <pedropaulo062011@gmail.com>
2024-09-07 15:07:45 -0300

    Aula 3 Imersão Google Gemini Alura

---
+++ AluraGemini_Set_2024/dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        titulo : "Elden Ring",
+        descricao : "Elden Ring é um RPG de ação em mundo aberto, desenvolvido pela FromSoftware, que te coloca no papel de um 'Maculado', um exilado em busca de restaurar o Anel Prístino e se tornar o Lorde Prístino.",
+        steam : "https://store.steampowered.com/app/1245620/ELDEN_RING/"
+    },
+    {
+        titulo : "Terraria",
+        descricao : "Terraria é um jogo de ação e aventura 2D com elementos de construção e exploração, onde você pode criar o seu próprio mundo e sobreviver a diversos perigos.",
+        steam : "https://store.steampowered.com/app/105600/Terraria/"
+    },
+    {
+        titulo : "Half Life 1",
+        descricao : "Half-Life 1 é um clássico dos jogos de tiro em primeira pessoa, considerado um dos pioneiros do gênero. Acompanhe Gordon Freeman em sua jornada para escapar de um laboratório de pesquisa após um experimento dar errado.",
+        steam : "https://store.steampowered.com/app/70/Half_Life/"
+    }
+];
