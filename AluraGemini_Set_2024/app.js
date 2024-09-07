ba5ef476db57b684a4c4a2b5ea921ba090f23996
Pedro Paulo <pedropaulo062011@gmail.com>
2024-09-07 17:26:14 -0300

    Aula 5 Imersão Google Gemini Alura

--- AluraGemini_Set_2024/app.js
+++ AluraGemini_Set_2024/app.js
@@ -9,20 +9,32 @@ function pesquisar() {
     // Seleciona a seção HTML onde os resultados serão exibidos.
     let section = document.getElementById("resultados-pesquisa");
   
+    // Seleciona o valor do campo de pesquisa (input);
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
     // Inicializa uma string vazia para armazenar o HTML dos resultados.
     let resultados = "";
   
+    if(!campoPesquisa){
+        section.innerHTML = "<p>Nada encontrado. Por favor, digite algo!<p>"
+        return;
+    }
+
     // Itera sobre os dados e constrói o HTML para cada resultado.
     for (let dado of dados) {
-      resultados += `
-        <div class="item-resultado">
-          <h2>${dado.titulo}</h2>
-          <p class="descricao-meta">${dado.descricao}</p>
-          <a href="${dado.steam}" target="_blank">Jogo na Steam</a>
-        </div>
-      `;
+        if(dado.titulo.toLowerCase().includes(campoPesquisa.toLowerCase()) || dado.tags.toLowerCase().includes(campoPesquisa.toLowerCase())){
+            resultados += `
+                <div class="item-resultado">
+                <h2>${dado.titulo}</h2>
+                <p class="descricao-meta">${dado.descricao}</p> <br>
+                <a href="${dado.steam}" target="_blank"><img src="steam-logo.png" alt="Logo Steam"></a>
+                </div>
+            `;
+        }
     }
   
+    if(!resultados) resultados = "<p>Nada encontrado...</p>"
+    
     // Atualiza o conteúdo HTML da seção com os resultados construídos.
     section.innerHTML = resultados;
   }
--- AluraGemini_Set_2024/dados.js
+++ AluraGemini_Set_2024/dados.js
@@ -2,16 +2,19 @@ let dados = [
     {
         titulo : "Elden Ring",
         descricao : "Elden Ring é um RPG de ação em mundo aberto, desenvolvido pela FromSoftware, que te coloca no papel de um 'Maculado', um exilado em busca de restaurar o Anel Prístino e se tornar o Lorde Prístino.",
-        steam : "https://store.steampowered.com/app/1245620/ELDEN_RING/"
+        steam : "https://store.steampowered.com/app/1245620/ELDEN_RING/",
+        tags : "maculado rpg fromsoftware from anel pristino mundo aberto lorde ação boss dificil difícil"
     },
     {
         titulo : "Terraria",
         descricao : "Terraria é um jogo de ação e aventura 2D com elementos de construção e exploração, onde você pode criar o seu próprio mundo e sobreviver a diversos perigos.",
-        steam : "https://store.steampowered.com/app/105600/Terraria/"
+        steam : "https://store.steampowered.com/app/105600/Terraria/",
+        tags : "sandbox 2d contrução construir exploração explorar sobreviver sobrevivência sobrevivencia blocos terras"
     },
     {
         titulo : "Half Life 1",
         descricao : "Half-Life 1 é um clássico dos jogos de tiro em primeira pessoa, considerado um dos pioneiros do gênero. Acompanhe Gordon Freeman em sua jornada para escapar de um laboratório de pesquisa após um experimento dar errado.",
-        steam : "https://store.steampowered.com/app/70/Half_Life/"
+        steam : "https://store.steampowered.com/app/70/Half_Life/",
+        tags : "tiro fps primeira pessoa gordon freeman freman friman alien zumbi monstro half life"
     }
 ];
