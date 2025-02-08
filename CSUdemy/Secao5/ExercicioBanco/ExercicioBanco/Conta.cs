b591c6b5c44aa0c0b247ebbc16db1f76b056aad5
cecilia cerqueira <ceciliacerqueira124@gmail.com>
2025-02-08 00:43:05 -0300

    Exercicio desafio da seção 5

---
+++ CSUdemy/Secao5/ExercicioBanco/ExercicioBanco/Conta.cs
@@ -0,0 +1,66 @@
+﻿using System.Globalization;
+
+namespace ExercicioBanco
+{
+    class Conta
+    {
+        private string _numeroConta;
+        private string _nomeTitularConta;
+        public double Saldo { get; private set; }
+
+        public Conta(string numeroConta, string nomeTitular)
+        {
+            _numeroConta = numeroConta;
+            _nomeTitularConta = nomeTitular;
+        }
+
+        public Conta(string numeroConta, string nomeTitular, double depositoInicial) : this(numeroConta, nomeTitular)
+        {
+            Depositar(depositoInicial);
+        }
+
+        public string NumeroConta
+        {
+            get { return _numeroConta; }
+            set
+            {
+                if (value != null && value.Length > 0)
+                {
+                    _numeroConta = value;
+                }
+            }
+        }
+
+        public string NomeTitularConta
+        {
+            get { return _nomeTitularConta; }
+            set
+            {
+                if (value != null && value.Length > 0)
+                {
+                    _nomeTitularConta = value;
+                }
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Sacar(double valor)
+        {
+            Saldo -= valor + 5.0; //O +5 é a taxa do banco kkkk
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+                + NumeroConta
+                + ", Titular: "
+                + NomeTitularConta
+                + ", Saldo: $ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ CSUdemy/Secao5/ExercicioBanco/ExercicioBanco/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+
+namespace ExercicioBanco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta;
+
+            Console.Write("Entre o número da conta: ");
+            string numeroConta = Console.ReadLine();
+            Console.Write("Entre o titular da conta: ");
+            string nomeTitular = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial? (s/n) ");
+            if(char.Parse(Console.ReadLine()) == 's')
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new Conta(numeroConta, nomeTitular, depositoInicial);
+            }
+            else
+            {
+                conta = new Conta(numeroConta, nomeTitular);
+            }
+
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
+
+            Console.Write("Entre um valor para depósito: ");
+            conta.Depositar(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+
+            Console.Write("Entre um valor para saque: ");
+            conta.Sacar(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+        }
+    }
+}
