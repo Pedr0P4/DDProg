062fe4e8a74a36c424c2b25ae6603c8270c4b666
Pedro Paulo <pedropaulo062011@gmail.com>
2025-02-08 01:49:56 -0300

    Exercicio de vetores da seção 6

---
+++ CSUdemy/Secao6/ExercicioVetor/ExercicioVetor/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ExercicioVetor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many rooms will be rented? ");
+            int rents = int.Parse(Console.ReadLine());
+
+            Student[] rooms = new Student[10];
+
+            for (int i = 0; i < rents; i++)
+            {
+                Console.WriteLine("Rent #" + (i+1) + ":");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("E-mail: ");
+                string email = Console.ReadLine();
+                Console.Write("Room: ");
+                int room = int.Parse(Console.ReadLine());
+
+                if(room >= 0 && room <= 9)
+                {
+                    rooms[room] = new Student(name, email, room);
+                }
+                else
+                {
+                    Console.WriteLine("Quarto " + room + " indisponível (0-9)");
+                }
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                if(rooms[i] != null)
+                {
+                    Console.WriteLine(rooms[i]);
+                }
+            } 
+        }
+    }
+}
---
+++ CSUdemy/Secao6/ExercicioVetor/ExercicioVetor/Student.cs
@@ -0,0 +1,45 @@
+﻿namespace ExercicioVetor
+{
+    class Student
+    {
+        private string _name;
+        private string _email;
+        public int Room { get; set; }
+
+        public Student(string name, string email, int room)
+        {
+            _name = name;
+            _email = email;
+            Room = room;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value != null && value.Length > 0)
+                {
+                    _name = value;
+                }
+            }
+        }
+
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                if (value != null && value.Length > 0)
+                {
+                    _email = value;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return Room + ": " + Name + ", " + Email;
+        }
+    }
+}
