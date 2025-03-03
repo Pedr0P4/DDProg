20f550183aab21bda74b4efb75ea44ad3e3984f3
Pedro Paulo <pedropaulo062011@gmail.com>
2025-03-02 22:28:46 -0300

    Exercício de arquivos

---
+++ CSUdemy/Secao11/ExercicioArquivos/ExercicioArquivos/Entities/Order.cs
@@ -0,0 +1,21 @@
+﻿namespace ExercicioArquivos.Entities
+{
+    class Order
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Amount { get; set; }
+
+        public Order(string name, double price, int amount)
+        {
+            Name = name;
+            Price = price;
+            Amount = amount;
+        }
+
+        public double TotalPrice()
+        {
+            return (double)Price * Amount;
+        }
+    }
+}
---
+++ CSUdemy/Secao11/ExercicioArquivos/ExercicioArquivos/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+using System.IO;
+
+using ExercicioArquivos.Entities;
+
+namespace ExercicioArquivos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the full file path:");
+            string sourceFilePath = Console.ReadLine();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourceFilePath);
+
+                string sourceFolderPath = Path.GetDirectoryName(sourceFilePath);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\summary.csv";
+
+                Directory.CreateDirectory(targetFolderPath);
+
+                using(StreamWriter sw = File.CreateText(targetFilePath))
+                {
+                    foreach(string line in lines)
+                    {
+                        string[] fields = line.Split(',');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int amount = int.Parse(fields[2]);
+
+                        Order order = new Order(name, price, amount);
+                        sw.WriteLine($"{order.Name},{order.TotalPrice().ToString("F2", CultureInfo.InvariantCulture)}");
+                    }
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error ocurred:");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
