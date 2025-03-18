d6b9fb8a11a30d676b3f2ef27394891f0ec4360b
Pedro Paulo <pedropaulo062011@gmail.com>
2025-03-17 23:11:12 -0300

    App mosquito continuação

--- CursoUdemyWeb/App_MataMosquito/js/script.js
+++ CursoUdemyWeb/App_MataMosquito/js/script.js
@@ -1,5 +1,6 @@
 let altura = 0;
 let largura = 0;
+let vidas = 3;
 
 function UpdateSize() {
     altura = window.innerHeight;
@@ -10,15 +11,29 @@ function UpdateSize() {
 UpdateSize();
 
 function DrawMosquito(){
+	
+	if(document.getElementById("mosquito")){
+		document.getElementById("mosquito").remove();
+		if(vidas > 0){
+			document.getElementById("v" + vidas--).src = "../imagens/coracao_vazio.png";
+		} else {
+			window.alert("Game over!");
+		}
+	}
+
     let positionX = Math.abs(Math.floor(Math.random() * largura) - 90);
     let positionY = Math.abs(Math.floor(Math.random() * altura) - 90);
 
     let mosquito = document.createElement("img");
     mosquito.src = "imagens/mosquito.png";
     mosquito.className = RandomizarClasse() + " " + RandomizarLado();
+	mosquito.id = "mosquito";
     mosquito.style.position = "absolute";
     mosquito.style.left = positionX + "px";
     mosquito.style.top = positionY + "px";
+	mosquito.onclick = function() {
+		this.remove();
+	}
 
     document.body.appendChild(mosquito);
     console.log(positionX, positionY);
@@ -34,4 +49,6 @@ function RandomizarLado() {
     return lado;
 }
 
-DrawMosquito();
+setInterval(function() {
+	DrawMosquito();
+}, 1000);
