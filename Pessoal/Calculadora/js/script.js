f2e2b118a823d2df9186531587376d28d6130980
Pedro Paulo <pedropaulo062011@gmail.com>
2025-03-25 14:01:11 -0300

    Funções de apagar, clear e parte de inverter sinal feito

---
+++ Pessoal/Calculadora/js/script.js
@@ -0,0 +1,79 @@
+let visor = document.getElementById("visor");
+let conteudo_visor = visor.value;
+
+let canOperate = false;
+
+let bracketsOpen = 0;
+
+function MidAppend(str, posToAdd, strToAdd) {
+	if(str === undefined || str.trim() === "") return null;
+	if(posToAdd > str.length) return null;
+
+	let pt1 = str.slice(0, posToAdd+1);
+	let pt2 = str.slice(posToAdd+1, str.length);
+
+	return pt1 + strToAdd + pt2;
+}
+
+function MidRemove(str, pos1, pos2) {
+	if(str === undefined || str.trim() === "") return null;
+	if(pos1 > pos2 && pos1 > str.length) return null;
+	if(pos2 > str.length) return null;
+
+	let pt1 = str.slice(0, pos1+1);
+	let pt2 = str.slice(pos2, str.length);
+	
+	return pt1 + pt2;
+}
+
+function digitar(digito) {
+	visor.value += digito;
+	canOperate = true;
+}
+
+function digitarOperador(operador) {
+	if(canOperate){
+		visor.value += ` ${operador} `;
+		canOperate = false;
+	}	
+}
+
+function invertSignal() {
+	let valor = visor.value;
+
+	if(valor.trim() == "") return;
+
+	if(canOperate){
+		for(let i = valor.length-1; i > 0; i--){
+			if(bracketsOpen > 0 && valor[i] === "-" && valor[i-1] === "(") {
+				visor.value = MidRemove(valor, i-1, i+1);
+				break;
+			} else if(bracketsOpen > 0 && valor[i] === "(") {
+				visor.value = MidAppend(valor, i, "-");
+				break;
+			}	
+		}
+	}
+}
+
+function openCloseBracket() {
+	if(visor.value.trim() == "") return;
+
+	if(!canOperate){
+		visor.value += "(";
+		bracketsOpen++;
+	} else if(bracketsOpen > 0){
+		visor.value += ")";
+		bracketsOpen--;
+	}
+}
+
+function apagar() {
+	let valor = visor.value;
+	if(valor[valor.length-1] == " ") visor.value = valor.slice(0, -3);
+	else visor.value = valor.slice(0, -1);
+}
+
+function clearBt() {
+	visor.value = "";
+}
