7b108bb20db41a76111fcf551e72343351b4b89f
Pedro Paulo <pedropaulo062011@gmail.com>
2025-03-27 13:55:02 -0300

    Boa parte já feita do projeto de orçamento pessoal

--- CursoUdemyWeb/App_OrcamentoPessoal/js/script.js
+++ CursoUdemyWeb/App_OrcamentoPessoal/js/script.js
@@ -7,6 +7,16 @@ class Despesa {
 		this.descricao = descricao;
 		this.valor = valor;
 	}
+
+	validarDados(){
+		for(let i in this){
+			if(this[i] === undefined || this[i].trim() === "" || this[i] === null){
+				return false;
+			}
+		}
+
+		return true;
+	}
 }
 
 class Bd {
@@ -26,6 +36,50 @@ class Bd {
 
 		localStorage.setItem("id", id);
 		localStorage.setItem(id, JSON.stringify(d));
+	}
+
+	recuperarTodosRegistros() {
+		let id = localStorage.getItem("id");
+		let despesas = Array();
+
+		for(let i = 1; i <= id; i++){
+			let despesa = JSON.parse(localStorage.getItem(i));
+			if(despesa === null) continue;
+			despesas.push(despesa);
+		}
+
+		return despesas;
+	}
+
+	pesquisar(d) {
+		let despesasFiltradas = Array();
+		despesasFiltradas = this.recuperarTodosRegistros();
+
+		if(d.ano != ""){
+			despesasFiltradas = despesasFiltradas.filter(despesa => despesa.ano == d.ano);
+		}
+
+		if(d.mes != ""){
+			despesasFiltradas = despesasFiltradas.filter(despesa => despesa.mes == d.mes);
+		}
+
+		if(d.dia != ""){
+			despesasFiltradas = despesasFiltradas.filter(despesa => despesa.dia == d.dia);
+		}
+
+		if(d.tipo != ""){
+			despesasFiltradas = despesasFiltradas.filter(despesa => despesa.tipo == d.tipo);
+		}
+
+		if(d.descricao != ""){
+			despesasFiltradas = despesasFiltradas.filter(despesa => despesa.descricao == d.descricao);
+		}
+
+		if(d.valor != ""){
+			despesasFiltradas = despesasFiltradas.filter(despesa => despesa.valor == d.valor);
+		}
+
+		console.log(despesasFiltradas);
 	}
 }
 
@@ -47,10 +101,90 @@ function cadastrarDespesa() {
 		descricao.value,
 		valor.value
 	);
+	
+	let modalTitle = document.getElementById("ModalLabel");
+	let modalText = document.getElementById("ModalText");
+	let modalBackButton = document.getElementById("ModalBackButton");
+	if(despesa.validarDados()){
+		bd.gravar(despesa);
+		modalTitle.innerHTML = "Sucesso na gravação";
+		modalTitle.className = "modal-title text-success";
+		modalText.innerHTML = "Sucesso no registro da despesa!";
+		modalBackButton.className = "btn btn-secondary btn-success";
+		//Modal de sucesso
+		$("#modalDeGravacao").modal("show");
 
-	bd.gravar(despesa);
+		ano.value = "";
+		mes.value = "";
+		dia.value = "";
+		tipo.value = "";
+		descricao.value = "";
+		valor.value = "";
+	} else{
+		modalTitle.innerHTML = "Erro na gravação";
+		modalTitle.className = "modal-title text-danger";
+		modalText.innerHTML = "Há campos obrigatórios que não foram preenchidos...";
+		modalBackButton.className = "btn btn-secondary btn-danger";
+		//Modal de erro
+		$("#modalDeGravacao").modal("show");
+	}
 }
 
-function gravar(d) {
-	localStorage.setItem('despesa', JSON.stringify(d));
+function carregarListaDespesas(){
+	let despesas = Array();
+	despesas = bd.recuperarTodosRegistros();
+
+	let listaDespesas = document.getElementById("dados");
+	
+	despesas.forEach(function(d) {
+		let linha = listaDespesas.insertRow();
+		linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`;
+
+		switch(parseInt(d.tipo)){
+			case 1:
+				d.tipo = "Alimentação";
+				break;
+			case 2:
+				d.tipo = "Educação";
+				break;
+			case 3:
+				d.tipo = "Lazer";
+				break;
+			case 4:
+				d.tipo = "Saúde";
+				break;
+			case 5:
+				d.tipo = "Transporte";
+				break;
+			default:
+				d.tipo = undefined;
+				break;
+		}
+
+		linha.insertCell(1).innerHTML = d.tipo;
+		linha.insertCell(2).innerHTML = d.descricao;
+		linha.insertCell(3).innerHTML = d.valor;
+	});
+}
+
+function pesquisarDespesas(){
+	let ano = document.getElementById("ano");
+	let mes = document.getElementById("mes");
+	let dia = document.getElementById("dia");
+	let tipo = document.getElementById("tipo");
+	let descricao = document.getElementById("descricao");
+	let valor = document.getElementById("valor");
+
+	let despesa = new Despesa(
+		ano.value,
+		mes.value,
+		dia.value,
+		tipo.value,
+		descricao.value,
+		valor.value
+	);
+
+	console.log(despesa);
+
+	bd.pesquisar(despesa);	
 }
