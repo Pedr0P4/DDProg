67abbbb3218c26bdd16fdb1455497394d5db1961
Pedro Paulo <pedropaulo062011@gmail.com>
2025-04-11 14:32:24 -0300

    Criação das primeiras classes do projeto (Xadrez)

---
+++ CSUdemy/xadrez-console/xadrez-console/Program.cs
@@ -0,0 +1,13 @@
+﻿using tabuleiro;
+
+namespace xadrez_console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao pos = new Posicao(5, 6);
+            Console.WriteLine(pos);
+        }
+    }
+}
---
+++ CSUdemy/xadrez-console/xadrez-console/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,16 @@
+﻿namespace tabuleiro
+{
+    class Tabuleiro
+    {
+        public int linhas { get; set; }
+        public int colunhas { get; set; }
+        private Peca[,] pecas;
+
+        public Tabuleiro(int linhas, int colunhas)
+        {
+            this.linhas = linhas;
+            this.colunhas = colunhas;
+            pecas = new Peca[linhas, colunhas];
+        }
+    }
+}
